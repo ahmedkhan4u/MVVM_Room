911c83b6ed37ae3da7145a937869c2b4af389be6
Ahmad-PC <ahmedkhan871871@gmail.com>
2022-08-26 15:02:43 +0500

    MVVM Room

--- app/src/main/java/com/example/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Toast;
 
 import com.example.mvvmdemo.AppDatabase.User;
 import com.example.mvvmdemo.ViewModels.MainActivityViewModel;
@@ -24,7 +25,32 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         viewModel = new ViewModelProvider(this).get(UserViewModel.class);
-        
+
+        viewModel.getUserData().observe(this, users -> {
+            String text = "";
+            for (int i=0; i< users.size(); i++) {
+                text += users.get(i).getId() + " : " + users.get(i).getFirstName()
+                        + " " + users.get(i).getLastName() + "\n";
+            }
+            binding.text.setText(text);
+        });
+
+        binding.insert.setOnClickListener(view -> {
+            viewModel.insertUser(new User("Test", "User"));
+        });
+
+        binding.delete.setOnClickListener(view -> {
+            User user = new User("Ahmed", "Khan");
+            user.setId(2);
+            viewModel.deleteUser(user);
+        });
+
+        binding.update.setOnClickListener(view -> {
+            User user = new User("Updated", "Name");
+            user.setId(7);
+            viewModel.updateUser(user);
+        });
+
 
     }
 }
